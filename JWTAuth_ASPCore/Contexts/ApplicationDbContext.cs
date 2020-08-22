23c552feaadd587e6d4df6ea7da52e388eb1341f
RJ Cabagnot <rjcabagnot@hotmail.com>
2020-08-22 18:22:00 +0200

    initial commit

---
+++ JWTAuth_ASPCore/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using JWTAuth_ASPCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuth_ASPCore.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ JWTAuth_ASPCore/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace JWTAuth_ASPCore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
